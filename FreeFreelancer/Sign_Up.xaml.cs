d12ae30598c3581f94580cec737a5ec60728f967
MohammedCS <mohammed.cis.cs@gmail.com>
2022-11-12 15:29:35 +0200

    Add project files.

---
+++ FreeFreelancer/Sign_Up.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FreeFreelancer
+{
+    /// <summary>
+    /// Interaction logic for Sign_Up.xaml
+    /// </summary>
+    public partial class Sign_Up : Window
+    {
+        public Sign_Up()
+        {
+            InitializeComponent();
+        }
+
+        private void Sign_Up_Click_2(object sender, RoutedEventArgs e)
+        {
+            MainWindow page = new MainWindow();
+            page.Show();
+            this.Close();
+        }
+
+        private void Log_In_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
